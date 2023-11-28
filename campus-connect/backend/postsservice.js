fda8ecdc6371c9cbf63d2977efee52c0b29adf6e
Hui Wagner <huiwagne@uw.edu>
2023-11-28 14:46:33 -0800

    postsservice.js update

--- campus-connect/backend/postsservice.js
+++ campus-connect/backend/postsservice.js
@@ -26,13 +26,6 @@ var app = express(express.json);
 // Import the authentication middleware
 const { authenticateJWT, authorizeRole } = require('./authMiddleware');
 
-
-
-
-
-
-
-
 //WEBSERVICE #2: Post Service
 // handles manipulation of user posts
 // Methods:
@@ -47,17 +40,66 @@ const { authenticateJWT, authorizeRole } = require('./authMiddleware');
 // upvotes, and viewcount are set to default values(0)
 // URI: http://localhost:port/posts
 
-app.post("/posts", (request, response) => {
-    const sqlQuery = "INSERT INTO posts VALUES (?);";
+// app.post("/posts", (request, response) => {
+//     const sqlQuery = "INSERT INTO posts VALUES (?);";
+//     const values = [
+//       request.body.subforumid,
+//       request.body.userid,
+//       request.body.title,
+//       request.body.content,
+//       request.body.postdate
+//     ];
+  
+//     dbConnection.query(sqlQuery, [values], (err, result) => {
+//       if (err) {
+//         return response
+//           .status(400)
+//           .json({ Error: "Failed: post was not added." });
+//       }
+//       return response
+//         .status(200)
+//         .json({ Success: "Successful: post was added!." });
+//     });
+//   });
+
+  // app.post("/posts", authenticateJWT, authorizeRole([1,2,3,4]), (request, response) => {
+  //   const sqlQuery = "INSERT INTO posts VALUES (?);";
+  //   const values = [
+  //     request.body.subforumid,
+  //     request.body.userid,
+  //     request.body.title,
+  //     request.body.content,
+  //     request.body.postdate
+  //   ];
+  
+  //   dbConnection.query(sqlQuery, [values], (err, result) => {
+  //     if (err) {
+  //       return response
+  //         .status(400)
+  //         .json({ Error: "Failed: post was not added." });
+  //     }
+  //     return response
+  //       .status(200)
+  //       .json({ Success: "Successful: post was added!." });
+  //   });
+  // });
+
+  //All user type can make a post.
+  app.post("/posts", authenticateJWT, (request, response) => {
+    // You might not need to get the userid from the request body as it can be extracted from the token
+    // const userid = request.user.userId; // Extracted from the JWT after authentication
+    
+    // Ensure you are inserting into the correct columns and that they match your database schema
+    const sqlQuery = "INSERT INTO posts (SubForumId, UserId, Title, Content, PostDate) VALUES (?, ?, ?, ?, ?);";
     const values = [
       request.body.subforumid,
-      request.body.userid,
+      request.user.userId, // Use userId from JWT
       request.body.title,
       request.body.content,
-      request.body.postdate
+      new Date() // You can set the postdate to the current time if that's what you want
     ];
   
-    dbConnection.query(sqlQuery, [values], (err, result) => {
+    dbConnection.query(sqlQuery, values, (err, result) => {
       if (err) {
         return response
           .status(400)
@@ -65,19 +107,10 @@ app.post("/posts", (request, response) => {
       }
       return response
         .status(200)
-        .json({ Success: "Successful: post was added!." });
+        .json({ Success: "Successful: post was added!", postId: result.insertId });
     });
   });
-
-  // // For routes accessible by admins and moderators
-  // app.delete("/posts/:postid", authenticateJWT, authorizeRole([2, 3]), (req, res) => {
-  //   // Only admins and moderators can reach this point
-  //   // Deletion logic here
-  // });
-
-
-
-
+  
 
   
   // ----------------------------------------------
