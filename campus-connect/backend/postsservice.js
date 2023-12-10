adba5563f91dcf810dc367e3a06f1456724e759c
alexgarcia941 <alexgarcia28098@gmail.com>
2023-12-09 16:17:00 -0800

    changes to git ignore and finished post service

--- campus-connect/backend/postsservice.js
+++ campus-connect/backend/postsservice.js
@@ -1,12 +1,8 @@
 // ----------------------------------------------
 // TCSS 460: Autumn 2023
+// Author: Alex Garcia 
 // Backend REST Service Module
-// ----------------------------------------------
-// Express is a Node.js web application framework
-// that provides a wide range of APIs and methods
-// Express API Reference:
-// https://expressjs.com/en/resources/middleware/cors.html
-
+// Post Service
 // ----------------------------------------------
 // retrieve necessary files (express and cors)
 const express = require("express");
@@ -16,6 +12,7 @@ const dbConnection = require("./config");
 // use this library for parsing HTTP body requests
 var bodyParser = require("body-parser");
 
+require('dotenv').config();
 // ----------------------------------------------
 // (A)  Create an express application instance
 //      and parses incoming requests with JSON
@@ -25,78 +22,25 @@ var app = express(express.json);
 
 // Import the authentication middleware
 const { authenticateJWT, authorizeRole } = require('./authMiddleware');
+const { JsonWebTokenError } = require("jsonwebtoken");
 
-//WEBSERVICE #2: Post Service
-// handles manipulation of user posts
-// Methods:
-// POST: Create a new post
-// GET: Retrieve post details
-// PATCH: Edit an existing post
-// DELETE: Remove a post
 
-// ----------------------------------------------
-// (1) create a new post
-// Note: post id is auto incremented so no need to pass in, 
-// upvotes, and viewcount are set to default values(0)
-// URI: http://localhost:port/posts
+//Routes
 
-// app.post("/posts", (request, response) => {
-//     const sqlQuery = "INSERT INTO posts VALUES (?);";
-//     const values = [
-//       request.body.subforumid,
-//       request.body.userid,
-//       request.body.title,
-//       request.body.content,
-//       request.body.postdate
-//     ];
-  
-//     dbConnection.query(sqlQuery, [values], (err, result) => {
-//       if (err) {
-//         return response
-//           .status(400)
-//           .json({ Error: "Failed: post was not added." });
-//       }
-//       return response
-//         .status(200)
-//         .json({ Success: "Successful: post was added!." });
-//     });
-//   });
+  // ----------------------------------------------
+  // (1) create a new post
+  // root URI: http://localhost:port//campus-connect/posts
+  // no auth required
+  app.post("/campus-connect/posts", authenticateJWT, authorizeRole([1,2,3]), (request, response) => {
 
-  // app.post("/posts", authenticateJWT, authorizeRole([1,2,3,4]), (request, response) => {
-  //   const sqlQuery = "INSERT INTO posts VALUES (?);";
-  //   const values = [
-  //     request.body.subforumid,
-  //     request.body.userid,
-  //     request.body.title,
-  //     request.body.content,
-  //     request.body.postdate
-  //   ];
-  
-  //   dbConnection.query(sqlQuery, [values], (err, result) => {
-  //     if (err) {
-  //       return response
-  //         .status(400)
-  //         .json({ Error: "Failed: post was not added." });
-  //     }
-  //     return response
-  //       .status(200)
-  //       .json({ Success: "Successful: post was added!." });
-  //   });
-  // });
-
-  //All user type can make a post.
-  app.post("/posts", authenticateJWT, (request, response) => {
-    // You might not need to get the userid from the request body as it can be extracted from the token
-    // const userid = request.user.userId; // Extracted from the JWT after authentication
-    
-    // Ensure you are inserting into the correct columns and that they match your database schema
+    // Insert sql statement
     const sqlQuery = "INSERT INTO posts (SubForumId, UserId, Title, Content, PostDate) VALUES (?, ?, ?, ?, ?);";
     const values = [
       request.body.subforumid,
       request.user.userId, // Use userId from JWT
       request.body.title,
       request.body.content,
-      new Date() // You can set the postdate to the current time if that's what you want
+      new Date() //current time
     ];
   
     dbConnection.query(sqlQuery, values, (err, result) => {
@@ -111,12 +55,12 @@ const { authenticateJWT, authorizeRole } = require('./authMiddleware');
     });
   });
   
-
   
   // ----------------------------------------------
-  // (2) retrive the information for a specific post
-  // root URI: http://localhost:port/posts/:postid
-  app.get("/posts/:postid", (request, response) => {
+  // (1) retrive the information for a specific post
+  // root URI: http://localhost:port/campus-connect/posts/:postid
+  // no auth required
+  app.get("/campus-connect/posts/:postid", (request, response) => {
     const postid = request.params.postid;
     const sqlQuery = "SELECT * FROM posts WHERE postid = '" + postid + "';";
     dbConnection.query(sqlQuery, (err, result) => {
@@ -130,19 +74,58 @@ const { authenticateJWT, authorizeRole } = require('./authMiddleware');
     });
   });
   
-  
+
   // ----------------------------------------------
-  // (3) update the info for a post
-  app.put("/posts/:postid", (request, response) => {
+  // (2) update the info and/or the title and content of a post only
+  //http://localhost:port/campus-connect/posts/:postid
+  // auth required, only original poster or admin allowed to edit, possibly a moderator
+  app.patch('/campus-connect/posts/:postid', authenticateJWT, authorizeRole([1,2]),  async (request,response) => {
     const postid = request.params.postid;
+
+    //If user is not an admin then check to see if they created the post
+    if(request.user.role == 1){
+      //retrieve the post info to check who created the post
+      url = `http://localhost:3000/campus-connect/posts/${postid}`
+      let postResponse = await fetch(url, {
+          method:'GET'
+      })
+
+      //retreive user id for creator
+      let postResponceData = await postResponse.json()
+      let post = postResponceData[0]
+      let creatorId = post["UserID"]
+
+      //check to see if userid matches to logged in user
+      if(creatorId != request.user.userId){
+        return response
+          .status(401)
+          .json({ Error: "Unathorized: invalid user" });
+      }
+    }
     
-    const sqlQuery = `UPDATE users SET content = ?
-      WHERE postid = ? ;`;
-    const values = [
-      request.body.content
-    ];
+    const newAtributes = request.body;
+    let values = [];
+
+    //build the update query
+    let sqlQuery = `UPDATE posts SET `
+    let temp = [];
+
+    //add attrubutes and values
+    for(var key in newAtributes){
+      if(key.toLocaleLowerCase() == "title" || key.toLocaleLowerCase() == "content"){
+        let attr = key + ` =  ?` ;
+        temp.push(attr);
+        values.push(newAtributes[key]);
+      }else{
+        return response
+               .status(400)
+               .json({ Error: "Failed: invalid attributes, please only use title and/or content =" });
+      }
+    }
+    let setString = temp.join(`, `);
+    sqlQuery += setString + ` WHERE postid = ?`
   
-    console.log(sqlQuery); // for debugging purposes:
+   //console.log(sqlQuery); // for debugging purposes:
     dbConnection.query(sqlQuery, [...values, postid], (err, result) => {
       if (err) {
         return response
@@ -153,13 +136,37 @@ const { authenticateJWT, authorizeRole } = require('./authMiddleware');
         .status(200)
         .json({ Success: "Successful: Post was edited!." });
     });
-  });
+  })
+
   
   // ----------------------------------------------
   // (4) Delete a post by post id
-  // make sure we designate username to be unique in database
-  app.delete("/posts/:postid", (request, response) => {
+  //only original post creator whould allowed to delete the post
+  app.delete("/campus-connect/posts/:postid",authenticateJWT, authorizeRole([1,2]), async (request, response) => {
     const postid = request.params.postid;
+
+    //If user is not an admin then check to see if they created the post
+    if(request.user.role == 1){
+      //retrieve the post info to check who created the post
+      url = `http://localhost:3000/campus-connect/posts/${postid}`
+      let postResponse = await fetch(url, {
+          method:'GET'
+      })
+
+      //retreive user id for creator
+      let postResponceData = await postResponse.json()
+      let post = postResponceData[0]
+      let creatorId = post["UserID"]
+
+      //check to see if userid matches to logged in user
+      if(creatorId != request.user.userId){
+        return response
+          .status(401)
+          .json({ Error: "Unathorized: invalid user" });
+      }
+    }
+    
+    //proceed with deletion
     const sqlQuery = "DELETE FROM posts WHERE postid = ? ; ";
     dbConnection.query(sqlQuery, postid, (err, result) => {
       if (err) {
