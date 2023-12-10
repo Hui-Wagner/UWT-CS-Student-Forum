3f81b8efffb01c76d72481cdc095b653cbe45eb7
Charmel Mbala <charmelmbala@gmail.com>
2023-12-10 05:06:24 -0800

    Update files

--- campus-connect/backend/postsservice.js
+++ campus-connect/backend/postsservice.js
@@ -1,18 +1,8 @@
 // ----------------------------------------------
 // TCSS 460: Autumn 2023
-<<<<<<< HEAD
-// Backend REST Service Module
-// ----------------------------------------------
-// Express is a Node.js web application framework
-// that provides a wide range of APIs and methods
-// Express API Reference:
-// https://expressjs.com/en/resources/middleware/cors.html
-
-=======
 // Author: Alex Garcia 
 // Backend REST Service Module
 // Post Service
->>>>>>> b11e81e8824163f8eb7be9909e41863075fcd2f2
 // ----------------------------------------------
 // retrieve necessary files (express and cors)
 const express = require("express");
@@ -22,10 +12,7 @@ const dbConnection = require("./config");
 // use this library for parsing HTTP body requests
 var bodyParser = require("body-parser");
 
-<<<<<<< HEAD
-=======
 require('dotenv').config();
->>>>>>> b11e81e8824163f8eb7be9909e41863075fcd2f2
 // ----------------------------------------------
 // (A)  Create an express application instance
 //      and parses incoming requests with JSON
@@ -33,32 +20,6 @@ require('dotenv').config();
 // ----------------------------------------------
 var app = express(express.json);
 
-<<<<<<< HEAD
-//WEBSERVICE #2: Post Service
-// handles manipulation of user posts
-// Methods:
-// POST: Create a new post
-// GET: Retrieve post details
-// PATCH: Edit an existing post
-// DELETE: Remove a post
-
-// ----------------------------------------------
-// (1) create a new post
-// Note: post id is auto incremented so no need to pass in, 
-// upvotes, and viewcount are set to default values(0)
-// URI: http://localhost:port/posts
-app.post("/posts", (request, response) => {
-    const sqlQuery = "INSERT INTO posts VALUES (?);";
-    const values = [
-      request.body.subforumid,
-      request.body.userid,
-      request.body.title,
-      request.body.content,
-      request.body.postdate
-    ];
-  
-    dbConnection.query(sqlQuery, [values], (err, result) => {
-=======
 // Import the authentication middleware
 const { authenticateJWT, authorizeRole } = require('./authMiddleware');
 const { JsonWebTokenError } = require("jsonwebtoken");
@@ -83,7 +44,6 @@ const { JsonWebTokenError } = require("jsonwebtoken");
     ];
   
     dbConnection.query(sqlQuery, values, (err, result) => {
->>>>>>> b11e81e8824163f8eb7be9909e41863075fcd2f2
       if (err) {
         return response
           .status(400)
@@ -91,16 +51,6 @@ const { JsonWebTokenError } = require("jsonwebtoken");
       }
       return response
         .status(200)
-<<<<<<< HEAD
-        .json({ Success: "Successful: post was added!." });
-    });
-  });
-  
-  // ----------------------------------------------
-  // (2) retrive the information for a specific post
-  // root URI: http://localhost:port/posts/:postid
-  app.get("/posts/:postid", (request, response) => {
-=======
         .json({ Success: "Successful: post was added!", postId: result.insertId });
     });
   });
@@ -111,7 +61,6 @@ const { JsonWebTokenError } = require("jsonwebtoken");
   // root URI: http://localhost:port/campus-connect/posts/:postid
   // no auth required
   app.get("/campus-connect/posts/:postid", (request, response) => {
->>>>>>> b11e81e8824163f8eb7be9909e41863075fcd2f2
     const postid = request.params.postid;
     const sqlQuery = "SELECT * FROM posts WHERE postid = '" + postid + "';";
     dbConnection.query(sqlQuery, (err, result) => {
@@ -125,22 +74,6 @@ const { JsonWebTokenError } = require("jsonwebtoken");
     });
   });
   
-<<<<<<< HEAD
-  
-  // ----------------------------------------------
-  // (3) update the info for a post
-  // city URI: http://localhost:port/vinyl/:album/price
-  app.put("/posts/:postid", (request, response) => {
-    const postid = request.params.postid;
-    
-    const sqlQuery = `UPDATE users SET content = ?
-      WHERE postid = ? ;`;
-    const values = [
-      request.body.content
-    ];
-  
-    console.log(sqlQuery); // for debugging purposes:
-=======
 
   // ----------------------------------------------
   // (2) update the info and/or the title and content of a post only
@@ -193,7 +126,6 @@ const { JsonWebTokenError } = require("jsonwebtoken");
     sqlQuery += setString + ` WHERE postid = ?`
   
    //console.log(sqlQuery); // for debugging purposes:
->>>>>>> b11e81e8824163f8eb7be9909e41863075fcd2f2
     dbConnection.query(sqlQuery, [...values, postid], (err, result) => {
       if (err) {
         return response
@@ -204,16 +136,6 @@ const { JsonWebTokenError } = require("jsonwebtoken");
         .status(200)
         .json({ Success: "Successful: Post was edited!." });
     });
-<<<<<<< HEAD
-  });
-  
-  // ----------------------------------------------
-  // (4) Delete a post by post id
-  // make sure we designate username to be unique in database
-  // city URI: http://localhost:port/vinyl/album
-  app.delete("/posts/:postid", (request, response) => {
-    const postid = request.params.postid;
-=======
   })
 
   
@@ -245,7 +167,6 @@ const { JsonWebTokenError } = require("jsonwebtoken");
     }
     
     //proceed with deletion
->>>>>>> b11e81e8824163f8eb7be9909e41863075fcd2f2
     const sqlQuery = "DELETE FROM posts WHERE postid = ? ; ";
     dbConnection.query(sqlQuery, postid, (err, result) => {
       if (err) {
